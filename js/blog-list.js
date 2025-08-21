6cce061c4f961134f6a76377de0cc8fcd3e29ed9
Toritama321 <suzumetori4@gmail.com>
2025-08-21 11:37:53 +0900

    Update blog-list.js

--- js/blog-list.js
+++ js/blog-list.js
@@ -1,5 +1,5 @@
 // ===== 設定 =====
-const POSTS_JSON = joinBase('data/posts.json'); // 記事メタ一覧の場所
+const POSTS_JSON = joinBase('/torinosu/blog/posts.json'); // 記事メタ一覧の場所
 
 // ===== 小道具 =====
 const $  = (s, r=document) => r.querySelector(s);
@@ -103,5 +103,6 @@ function applyFilter(tag) {
     const em = $('#empty'); em.textContent = '記事一覧を取得できなかった…'; em.hidden = false;
   }
 })();
+
 
 
